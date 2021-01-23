601f37f05fcb23f841ff41e3ec050148d8688be3
Mouna <amroumouna@gmail.com>
2021-01-23 12:43:48 +0000

    feat: average listing selling price per seller type

--- src/main/scala/core/Main.scala
+++ src/main/scala/core/Main.scala
@@ -1,5 +1,24 @@
 package core
 
-object Main {
+import cats.data.NonEmptyList
+import cats.effect.{ExitCode, IO, IOApp}
+import com.github.gekomad.ittocsv.core.ParseFailure
+import com.github.gekomad.ittocsv.parser.IttoCSVFormat
+import com.github.gekomad.ittocsv.parser.io.FromFile.csvFromFileStream
+import datatypes.{Contact, Listing}
+import reports.Report1.averageSellingPrice
+
+object Main extends IOApp {
+
+ implicit val csvFormat: IttoCSVFormat = com.github.gekomad.ittocsv.parser.IttoCSVFormat.default
+
+ val listingStream: fs2.Stream[IO, Either[NonEmptyList[ParseFailure], Listing]] =
+  csvFromFileStream[Listing]("data/listings.csv", skipHeader = true).dropLast
+
+ val contactsStream: fs2.Stream[IO, Either[NonEmptyList[ParseFailure], Contact]] =
+  csvFromFileStream[Contact]("data/contacts.csv", skipHeader = true).dropLast
+
+ override def run(args: List[String]): IO[ExitCode] =
+  averageSellingPrice(listingStream).compile.drain.as(ExitCode.Success)
 
 }
--- src/main/scala/reports/Report1.scala
+++ src/main/scala/reports/Report1.scala
@@ -1,3 +1,25 @@
-package reports object Report1 {
+package reports
 
+import cats.data.NonEmptyList
+import cats.effect.IO
+import com.github.gekomad.ittocsv.core.ParseFailure
+import datatypes.Listing
+
+object Report1 {
+
+  /** calculating the average selling price per seller type
+   * @param stream Stream of Listing
+   * @return Map identified by Seller type and the associated average selling price
+   */
+  def averageSellingPrice(stream : fs2.Stream[IO, Either[NonEmptyList[ParseFailure], Listing]]): fs2.Stream[IO, Unit] = stream.fold(Map.empty[String, (Int, Int)]) {
+    (acc, listing) =>
+      listing match {
+        case Right(value) => acc.updatedWith(value.sellerType) {
+          previous => Some(previous.fold((value.price, 0))(prev => (prev._1 + value.price, prev._2 + 1)))
+        }
+        case Left(_) => acc
+      }
+  }
+    .map(entry => entry.map{ case (k,v) => Map(k -> v._1 / v._2)})
+    .evalMap(entries => IO(println(entries)))
 }
