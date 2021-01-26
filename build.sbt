71d3e65cd6744c668992da5e39b2728a4a10cd79
Mouna <amroumouna@gmail.com>
2021-01-26 10:51:05 +0000

    add: implicit support and fork

--- build.sbt
+++ build.sbt
@@ -4,11 +4,14 @@ version := "0.1"
 
 scalaVersion := "2.13.4"
 
+addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
+fork in run := true
+
 libraryDependencies ++= Seq(
   "ch.qos.logback" % "logback-classic" % "1.2.3",
   "io.chrisdavenport" %% "log4cats-slf4j" % "1.1.1",
   "com.github.gekomad" %% "itto-csv" % "1.1.0",
   "org.typelevel" %% "cats-effect" % "2.3.1",
   "de.vandermeer" % "asciitable" % "0.3.2",
-  "org.scalatest" %% "scalatest" % "3.1.0" % "test"
-  )
+  "org.scalatest" %% "scalatest" % "3.1.0" % Test
+)
---
+++ src/main/scala/reports/AverageListingSellingPrice.scala
@@ -0,0 +1,44 @@
+package Reports
+
+import cats.effect.IO
+import de.vandermeer.asciitable.AsciiTable
+import fs2.Stream
+import io.chrisdavenport.log4cats.Logger
+
+object AverageListingSellingPrice {
+
+  import datatypes._
+
+  def report(listingStream: Stream[IO, Option[ Listing]])(implicit logger: Logger[IO]) =
+    listingStream.fold(Map.empty[String, (Int, Int)]) {
+      (acc, listing) => {
+        listing.map { listing => acc.updatedWith(listing.sellerType) {
+          previous => Some(previous.fold(listing.price, 1) {
+            case (price, count) => (price + listing.price, count + 1)})
+        }
+        }.getOrElse(acc)
+      }
+    }.map{ entry  => entry.map {
+      case (sellerType,(sumPrices, count)) => sellerType  -> (sumPrices / count)
+    }
+    }.through(output _)
+
+  def output(stream: Stream[IO, Map[String, Int]])(implicit logger: Logger[IO]): Stream[IO, Unit] = {
+    val at = new AsciiTable()
+
+    at.addRule()
+    at.addRow("Seller Type", "Average in Euro")
+
+    stream.evalMap { entry =>
+      entry.foreach {
+        case (sellerType, amount) =>
+          at.addRule()
+          at.addRow(sellerType, "€" +amount.toString + ",-")
+      }
+      at.addRule()
+
+      logger.info(s"\n${at.render()}")
+
+    }
+  }
+}
